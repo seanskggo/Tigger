f4ec61c6ae0f326d23580eb73576cf30931ef0dc
z5310199 <z5310199@cse.unsw.edu.au>
2022-07-05 18:44:51 +1000

    test progress

--- tests/test00.sh
+++ tests/test00.sh
@@ -1,2 +1,54 @@
 #!/bin/dash
 
+######################################################################
+# Test Script No. 0
+# Subset 0 
+# 
+# Testing commands:
+# - tigger-init 
+# - tigger-add 
+#
+# Usage: test00.sh <path to tigger-init> <path to tigger-add>
+######################################################################
+
+######################################################################
+# Create output file for implemented tigger commands
+######################################################################
+
+mkdir temp && cd temp 
+
+### TEST BEGIN ###
+
+../"$1" 2>&1 >> ../a 
+
+### TEST END ###
+
+cd .. && rm -rf temp 
+
+######################################################################
+# Create output file for 2041 tigger commands
+######################################################################
+
+mkdir temp && cd temp 
+
+### VALIDATION BEGIN ###
+
+2041 tigger-init 2>&1 >> ../b
+
+### VALIDATION END ###
+
+cd .. && rm -rf temp 
+
+######################################################################
+# OUtcome
+######################################################################
+
+if [ -z "$(diff a b)" ]
+then 
+    echo "PASSED"
+else
+    echo "FAILED"
+    diff a b
+fi
+
+rm a b 
