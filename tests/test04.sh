18adb3e46b53c37f2f09e51b7ef50bcd8f003a80
z5310199 <z5310199@cse.unsw.edu.au>
2022-07-07 21:27:05 +1000

    progress

--- tests/test04.sh
+++ tests/test04.sh
@@ -20,7 +20,7 @@ test_commands () {
 
     tigger-rm # should error => .tigger does not exist > usage error
     tigger-rm test # should error => .tigger does not exist > unknown file
-    tigger-init > /dev/null                         
+    tigger-init > /dev/null
     tigger-rm # should error => usage error              
     tigger-rm --force # should error => usage error         
     tigger-rm --force --cached # should error => usage error 
@@ -33,6 +33,31 @@ test_commands () {
     tigger-commit -m test > /dev/null               
     tigger-rm a # should succeed
     tigger-rm a # should fail after removal
+
+    # tigger-rm 
+    rm -rf .tigger && rm -f *
+    tigger-init > /dev/null
+    # Case 1: file and index differ (test both ways)
+    echo "test" > a
+    tigger-rm a
+    tigger-add a
+    tigger-rm a
+    # Case 2: file and commit differ (+ test with deleted file)
+    tigger-commit -m test > /dev/null
+    echo "changed" > a
+    tigger-rm a
+    rm a
+    tigger-rm a
+    # Case 3: commit and index differ
+    echo "new" > b
+    tigger-add b
+    tigger-commit -m test > /dev/null
+    echo "changed" > b
+    tigger-add b
+    tigger-rm b
+    # Case 4: commit, index file all differ
+    echo "changed again" > b
+    tigger-rm b
 
     cd .. && rm -rf temp
 
@@ -62,6 +87,31 @@ make_answers () {
     2041 tigger-commit -m test > /dev/null               
     2041 tigger-rm a 
     2041 tigger-rm a 
+
+    # tigger-rm 
+    rm -rf .tigger && rm -f *
+    2041 tigger-init > /dev/null
+    # Case 1: file and index differ (test both ways)
+    echo "test" > a
+    2041 tigger-rm a
+    2041 tigger-add a
+    2041 tigger-rm a
+    # Case 2: file and commit differ (+ test with deleted file)
+    2041 tigger-commit -m test > /dev/null
+    echo "changed" > a
+    2041 tigger-rm a
+    rm a
+    2041 tigger-rm a
+    # Case 3: commit and index differ
+    echo "new" > b
+    2041 tigger-add b
+    2041 tigger-commit -m test > /dev/null
+    echo "changed" > b
+    2041 tigger-add b
+    2041 tigger-rm b
+    # Case 4: commit, index file all differ
+    echo "changed again" > b
+    2041 tigger-rm b
 
     cd .. && rm -rf temp
 
