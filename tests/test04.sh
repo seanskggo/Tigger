c4cbbeb7f99c82d10874164f37e7a0a05cd2f7de
z5310199 <z5310199@cse.unsw.edu.au>
2022-07-08 12:53:28 +1000

    progress

--- tests/test04.sh
+++ tests/test04.sh
@@ -18,46 +18,46 @@ test_commands () {
 
     mkdir temp && cd temp
 
-    # tigger-rm # should error => .tigger does not exist > usage error
-    # tigger-rm test # should error => .tigger does not exist > unknown file
-    # tigger-init > /dev/null
-    # tigger-rm # should error => usage error              
-    # tigger-rm --force # should error => usage error         
-    # tigger-rm --force --cached # should error => usage error 
+    tigger-rm # should error => .tigger does not exist > usage error
+    tigger-rm test # should error => .tigger does not exist > unknown file
+    tigger-init > /dev/null
+    tigger-rm # should error => usage error              
+    tigger-rm --force # should error => usage error         
+    tigger-rm --force --cached # should error => usage error 
 
-    # tigger-rm a # should error => file not in repository
-    # touch a                                         
-    # tigger-rm a # should error => file not in repository
-    # tigger-add a                                     
-    # tigger-rm a # should error => file not in repository                               
-    # tigger-commit -m test > /dev/null               
-    # tigger-rm a # should succeed
-    # tigger-rm a # should fail after removal
+    tigger-rm a # should error => file not in repository
+    touch a                                         
+    tigger-rm a # should error => file not in repository
+    tigger-add a                                     
+    tigger-rm a # should error => file not in repository                               
+    tigger-commit -m test > /dev/null               
+    tigger-rm a # should succeed
+    tigger-rm a # should fail after removal
 
-    # # tigger-rm 
-    # rm -rf .tigger && rm -f *
-    # tigger-init > /dev/null
-    # # Case 1: file and index differ (test both ways)
-    # echo "test" > a
-    # tigger-rm a
-    # tigger-add a
-    # tigger-rm a
-    # # Case 2: file and commit differ (+ test with deleted file)
-    # tigger-commit -m test > /dev/null
-    # echo "changed" > a
-    # tigger-rm a
-    # rm a
-    # tigger-rm a
-    # # Case 3: commit and index differ
-    # echo "new" > b
-    # tigger-add b
-    # tigger-commit -m test > /dev/null
-    # echo "changed" > b
-    # tigger-add b
-    # tigger-rm b
-    # # Case 4: commit, index file all differ
-    # echo "changed again" > b
-    # tigger-rm b
+    # tigger-rm 
+    rm -rf .tigger && rm -f *
+    tigger-init > /dev/null
+    # Case 1: file and index differ (test both ways)
+    echo "test" > a
+    tigger-rm a
+    tigger-add a
+    tigger-rm a
+    # Case 2: file and commit differ (+ test with deleted file)
+    tigger-commit -m test > /dev/null
+    echo "changed" > a
+    tigger-rm a
+    rm a
+    tigger-rm a
+    # Case 3: commit and index differ
+    echo "new" > b
+    tigger-add b
+    tigger-commit -m test > /dev/null
+    echo "changed" > b
+    tigger-add b
+    tigger-rm b
+    # Case 4: commit, index file all differ
+    echo "changed again" > b
+    tigger-rm b
 
     # tigger-rm --cached
     rm -rf .tigger && rm -f *
@@ -66,13 +66,15 @@ test_commands () {
     echo "test" > a
     tigger-rm --cached a
     tigger-add a
+    echo "change" >> a
     tigger-rm --cached a
     # Case 2: file and commit differ (+ test with deleted file)
     tigger-commit -m test > /dev/null
-    echo "changed" > a
+    echo "changed 2" > a
     tigger-rm --cached a
-    rm a
-    tigger-rm --cached a
+    touch b
+    tigger-add b
+    tigger-rm --cached b
     # Case 3: commit and index differ
     echo "new" > b
     tigger-add b
@@ -81,7 +83,12 @@ test_commands () {
     tigger-add b
     tigger-rm --cached b
     # Case 4: commit, index file all differ
-    echo "changed again" > b
+    echo "new again" > b
+    tigger-add b
+    tigger-commit -m test > /dev/null
+    echo "changed" >> b
+    tigger-add b 
+    echo "changed again" >> b
     tigger-rm --cached b
 
     cd .. && rm -rf temp
@@ -97,46 +104,46 @@ make_answers () {
     
     mkdir temp && cd temp
 
-    # 2041 tigger-rm 
-    # 2041 tigger-rm test 
-    # 2041 tigger-init > /dev/null                         
-    # 2041 tigger-rm                                       
-    # 2041 tigger-rm --force                               
-    # 2041 tigger-rm --force --cached                      
-    # 2041 tigger-rm a                                     
+    2041 tigger-rm 
+    2041 tigger-rm test 
+    2041 tigger-init > /dev/null                         
+    2041 tigger-rm                                       
+    2041 tigger-rm --force                               
+    2041 tigger-rm --force --cached                      
+    2041 tigger-rm a                                     
 
-    # touch a                                         
-    # 2041 tigger-rm a                                     
-    # 2041 tigger-add a                                    
-    # 2041 tigger-rm a                                     
-    # 2041 tigger-commit -m test > /dev/null               
-    # 2041 tigger-rm a 
-    # 2041 tigger-rm a 
+    touch a                                         
+    2041 tigger-rm a                                     
+    2041 tigger-add a                                    
+    2041 tigger-rm a                                     
+    2041 tigger-commit -m test > /dev/null               
+    2041 tigger-rm a 
+    2041 tigger-rm a 
 
-    # # tigger-rm 
-    # rm -rf .tigger && rm -f *
-    # 2041 tigger-init > /dev/null
-    # # Case 1: file and index differ (test both ways)
-    # echo "test" > a
-    # 2041 tigger-rm a
-    # 2041 tigger-add a
-    # 2041 tigger-rm a
-    # # Case 2: file and commit differ (+ test with deleted file)
-    # 2041 tigger-commit -m test > /dev/null
-    # echo "changed" > a
-    # 2041 tigger-rm a
-    # rm a
-    # 2041 tigger-rm a
-    # # Case 3: commit and index differ
-    # echo "new" > b
-    # 2041 tigger-add b
-    # 2041 tigger-commit -m test > /dev/null
-    # echo "changed" > b
-    # 2041 tigger-add b
-    # 2041 tigger-rm b
-    # # Case 4: commit, index file all differ
-    # echo "changed again" > b
-    # 2041 tigger-rm b
+    # tigger-rm 
+    rm -rf .tigger && rm -f *
+    2041 tigger-init > /dev/null
+    # Case 1: file and index differ (test both ways)
+    echo "test" > a
+    2041 tigger-rm a
+    2041 tigger-add a
+    2041 tigger-rm a
+    # Case 2: file and commit differ (+ test with deleted file)
+    2041 tigger-commit -m test > /dev/null
+    echo "changed" > a
+    2041 tigger-rm a
+    rm a
+    2041 tigger-rm a
+    # Case 3: commit and index differ
+    echo "new" > b
+    2041 tigger-add b
+    2041 tigger-commit -m test > /dev/null
+    echo "changed" > b
+    2041 tigger-add b
+    2041 tigger-rm b
+    # Case 4: commit, index file all differ
+    echo "changed again" > b
+    2041 tigger-rm b
 
     # tigger-rm --cached
     rm -rf .tigger && rm -f *
@@ -145,13 +152,15 @@ make_answers () {
     echo "test" > a
     2041 tigger-rm --cached a
     2041 tigger-add a
+    echo "change" >> a
     2041 tigger-rm --cached a
     # Case 2: file and commit differ (+ test with deleted file)
     2041 tigger-commit -m test > /dev/null
-    echo "changed" > a
+    echo "changed 2" > a
     2041 tigger-rm --cached a
-    rm a
-    2041 tigger-rm --cached a
+    touch b
+    2041 tigger-add b
+    2041 tigger-rm --cached b
     # Case 3: commit and index differ
     echo "new" > b
     2041 tigger-add b
@@ -160,7 +169,12 @@ make_answers () {
     2041 tigger-add b
     2041 tigger-rm --cached b
     # Case 4: commit, index file all differ
-    echo "changed again" > b
+    echo "new again" > b
+    2041 tigger-add b
+    2041 tigger-commit -m test > /dev/null
+    echo "changed" >> b
+    2041 tigger-add b 
+    echo "changed again" >> b
     2041 tigger-rm --cached b
 
     cd .. && rm -rf temp
@@ -179,8 +193,11 @@ then
     echo "PASSED"
 else
     echo "FAILED"
-    diff a b
-    # diff -y a b | cat -n | grep -v -e '($'  
+    # diff a b
+    # cat a
+    # echo "-"
+    # cat b
+    diff -y a b | cat -n | grep -v -e '($'  
 fi
 
 rm a b
