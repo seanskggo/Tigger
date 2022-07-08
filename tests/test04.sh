e5472fa32225f9f27266e7034f68b45467457ff4
z5310199 <z5310199@cse.unsw.edu.au>
2022-07-08 11:09:39 +1000

    update

--- tests/test04.sh
+++ tests/test04.sh
@@ -18,46 +18,71 @@ test_commands () {
 
     mkdir temp && cd temp
 
-    tigger-rm # should error => .tigger does not exist > usage error
-    tigger-rm test # should error => .tigger does not exist > unknown file
-    tigger-init > /dev/null
-    tigger-rm # should error => usage error              
-    tigger-rm --force # should error => usage error         
-    tigger-rm --force --cached # should error => usage error 
+    # tigger-rm # should error => .tigger does not exist > usage error
+    # tigger-rm test # should error => .tigger does not exist > unknown file
+    # tigger-init > /dev/null
+    # tigger-rm # should error => usage error              
+    # tigger-rm --force # should error => usage error         
+    # tigger-rm --force --cached # should error => usage error 
 
-    tigger-rm a # should error => file not in repository
-    touch a                                         
-    tigger-rm a # should error => file not in repository
-    tigger-add a                                     
-    tigger-rm a # should error => file not in repository                               
-    tigger-commit -m test > /dev/null               
-    tigger-rm a # should succeed
-    tigger-rm a # should fail after removal
+    # tigger-rm a # should error => file not in repository
+    # touch a                                         
+    # tigger-rm a # should error => file not in repository
+    # tigger-add a                                     
+    # tigger-rm a # should error => file not in repository                               
+    # tigger-commit -m test > /dev/null               
+    # tigger-rm a # should succeed
+    # tigger-rm a # should fail after removal
 
-    # tigger-rm 
+    # # tigger-rm 
+    # rm -rf .tigger && rm -f *
+    # tigger-init > /dev/null
+    # # Case 1: file and index differ (test both ways)
+    # echo "test" > a
+    # tigger-rm a
+    # tigger-add a
+    # tigger-rm a
+    # # Case 2: file and commit differ (+ test with deleted file)
+    # tigger-commit -m test > /dev/null
+    # echo "changed" > a
+    # tigger-rm a
+    # rm a
+    # tigger-rm a
+    # # Case 3: commit and index differ
+    # echo "new" > b
+    # tigger-add b
+    # tigger-commit -m test > /dev/null
+    # echo "changed" > b
+    # tigger-add b
+    # tigger-rm b
+    # # Case 4: commit, index file all differ
+    # echo "changed again" > b
+    # tigger-rm b
+
+    # tigger-rm --cached
     rm -rf .tigger && rm -f *
     tigger-init > /dev/null
     # Case 1: file and index differ (test both ways)
     echo "test" > a
-    tigger-rm a
+    tigger-rm --cached a
     tigger-add a
-    tigger-rm a
+    tigger-rm --cached a
     # Case 2: file and commit differ (+ test with deleted file)
     tigger-commit -m test > /dev/null
     echo "changed" > a
-    tigger-rm a
+    tigger-rm --cached a
     rm a
-    tigger-rm a
+    tigger-rm --cached a
     # Case 3: commit and index differ
     echo "new" > b
     tigger-add b
     tigger-commit -m test > /dev/null
     echo "changed" > b
     tigger-add b
-    tigger-rm b
+    tigger-rm --cached b
     # Case 4: commit, index file all differ
     echo "changed again" > b
-    tigger-rm b
+    tigger-rm --cached b
 
     cd .. && rm -rf temp
 
@@ -72,46 +97,71 @@ make_answers () {
     
     mkdir temp && cd temp
 
-    2041 tigger-rm 
-    2041 tigger-rm test 
-    2041 tigger-init > /dev/null                         
-    2041 tigger-rm                                       
-    2041 tigger-rm --force                               
-    2041 tigger-rm --force --cached                      
-    2041 tigger-rm a                                     
+    # 2041 tigger-rm 
+    # 2041 tigger-rm test 
+    # 2041 tigger-init > /dev/null                         
+    # 2041 tigger-rm                                       
+    # 2041 tigger-rm --force                               
+    # 2041 tigger-rm --force --cached                      
+    # 2041 tigger-rm a                                     
 
-    touch a                                         
-    2041 tigger-rm a                                     
-    2041 tigger-add a                                    
-    2041 tigger-rm a                                     
-    2041 tigger-commit -m test > /dev/null               
-    2041 tigger-rm a 
-    2041 tigger-rm a 
+    # touch a                                         
+    # 2041 tigger-rm a                                     
+    # 2041 tigger-add a                                    
+    # 2041 tigger-rm a                                     
+    # 2041 tigger-commit -m test > /dev/null               
+    # 2041 tigger-rm a 
+    # 2041 tigger-rm a 
 
-    # tigger-rm 
+    # # tigger-rm 
+    # rm -rf .tigger && rm -f *
+    # 2041 tigger-init > /dev/null
+    # # Case 1: file and index differ (test both ways)
+    # echo "test" > a
+    # 2041 tigger-rm a
+    # 2041 tigger-add a
+    # 2041 tigger-rm a
+    # # Case 2: file and commit differ (+ test with deleted file)
+    # 2041 tigger-commit -m test > /dev/null
+    # echo "changed" > a
+    # 2041 tigger-rm a
+    # rm a
+    # 2041 tigger-rm a
+    # # Case 3: commit and index differ
+    # echo "new" > b
+    # 2041 tigger-add b
+    # 2041 tigger-commit -m test > /dev/null
+    # echo "changed" > b
+    # 2041 tigger-add b
+    # 2041 tigger-rm b
+    # # Case 4: commit, index file all differ
+    # echo "changed again" > b
+    # 2041 tigger-rm b
+
+    # tigger-rm --cached
     rm -rf .tigger && rm -f *
     2041 tigger-init > /dev/null
     # Case 1: file and index differ (test both ways)
     echo "test" > a
-    2041 tigger-rm a
+    2041 tigger-rm --cached a
     2041 tigger-add a
-    2041 tigger-rm a
+    2041 tigger-rm --cached a
     # Case 2: file and commit differ (+ test with deleted file)
     2041 tigger-commit -m test > /dev/null
     echo "changed" > a
-    2041 tigger-rm a
+    2041 tigger-rm --cached a
     rm a
-    2041 tigger-rm a
+    2041 tigger-rm --cached a
     # Case 3: commit and index differ
     echo "new" > b
     2041 tigger-add b
     2041 tigger-commit -m test > /dev/null
     echo "changed" > b
     2041 tigger-add b
-    2041 tigger-rm b
+    2041 tigger-rm --cached b
     # Case 4: commit, index file all differ
     echo "changed again" > b
-    2041 tigger-rm b
+    2041 tigger-rm --cached b
 
     cd .. && rm -rf temp
 
@@ -129,10 +179,7 @@ then
     echo "PASSED"
 else
     echo "FAILED"
-    # diff a b
-    cat a 
-    echo "-------------"
-    cat b
+    diff a b
     # diff -y a b | cat -n | grep -v -e '($'  
 fi
 
